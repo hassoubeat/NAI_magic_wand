869ae382db7322ea35f98e5f5eafaab63a8faa48
hassoubeat <hassoubeat.work@gmail.com>
2022-10-27 18:23:49 +0900

    Refactor split style.scss

--- src/js/popup/components/PromptsEditor/WorkingPrompts/index.vue
+++ src/js/popup/components/PromptsEditor/WorkingPrompts/index.vue
@@ -32,22 +32,23 @@
       @dragenter.prevent
     />
   </div>
-  <div class="add-prompt-container">
-    <button @click="addWorkPrompt">
-      + Add Prompt
-    </button>
-  </div>
+  <Actions
+    :addWorkPrompt="addWorkPrompt"
+    :copyWorkPromptsToClipboard="copyWorkPromptsToClipboard"
+  />
 </template>
  
 <script>
 
 
 import PromptEditor from "./PromptEditor";
+import Actions from "./Actions";
 import { useEnhancer } from "./enhancer";
  
 export default {
   components: {
-    PromptEditor
+    PromptEditor,
+    Actions
   },
   props: {
     workPrompts: {
@@ -66,70 +67,5 @@ export default {
 </script>
  
 <style lang="scss" scoped>
-  .work-prompts-container {
-    display: flex;
-    flex-direction: column;
-    gap: 2px;
-
-    .title {
-      font-size: 16px;
-    }
-    .work-prompts {
-      position: relative;
-      width: 100%;
-      height: 100px;
-      background-color: #f5f5f5;
-      border: 1px solid gray;
-      overflow-y: scroll;
-      box-sizing: border-box;
-
-      .prompts {
-        position: absolute;
-        left: 0;
-        top: 0;
-        z-index: 0;
-        padding: 2px 4px;
-        font-size: 16px;
-      }
-
-      .sticky-container {
-        position: sticky;
-        left: 0;
-        top: 0;
-        z-index: 1;
-        width: 100%;
-        height: 100%;
-
-        .copy-icon {
-          position: absolute;
-          bottom: 6px;
-          right: 6px;
-          display: inline-block;
-          background-image: url("~/src/image/icons/copy.png");
-          background-repeat: no-repeat;
-          background-size: cover;
-          width: 25px;
-          height: 25px;
-          cursor: pointer;
-        }
-      }
-    }
-  }
-  .prompts-editor-container{
-    display: flex;
-    flex-wrap: wrap;
-  }
-
-  .add-prompt-container {
-    button {
-      width: 100%;
-      font-size: 14px;
-      font-weight: 700;
-      padding: 4px;
-      background: white;
-      border: 1px solid gray;
-      cursor: pointer;
-    }
-  }
-  
+  @import './style.scss';
 </style>

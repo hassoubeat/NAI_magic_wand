f1b46b9dcf3359ab88602ea2e74290ed0d37056c
hassoubeat <hassoubeat.work@gmail.com>
2022-12-03 17:40:30 +0900

    Add const

--- src/js/common/const.js
+++ src/js/common/const.js
@@ -1,2 +1,4 @@
 export const PROMPTS_BOOKMARKS_LIMIT = 300;
+export const PROMPTS_BOOKMARKS_EXPORT_FILE_IDENTIFIER = "NAI_magic_wand prompts bookmarks export file";
+export const PROMPTS_BOOKMARKS_EXPORT_FILE_LATEST_VERSION = "1.0";
 export const NOVEL_AI_GENERATE_IMAGE_IDENTIER = "NovelAI";
